8348bb646f534cafe092e00c6bace84bbac60d36
evanjscallan <evanjscallan@gmail.com>
2022-04-16 16:06:33 -0500

    troubleshoot pt.4

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,7 +11,7 @@ import xMark from './../img/nav/x.svg'
 
 
 export default function Contact(props, contactState){
-	const [isInViewport, targetRef] = null
+	const [targetRef] = null
 	const setContactVisible = props.contactState
 	const backBtn=props.backBtn
 
